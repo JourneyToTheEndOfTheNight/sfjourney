10bc26b4f8d723d789f24a0d6a9be1bf08b9acf0
Thomas Lotze <lotze@squareup.com>
2013-09-23 15:10:57 -0700

    initial commit (with omniauth)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sfjourney::Application.config.secret_key_base = '29a34e41fb23f940e67f101b8bf5212d0500449f891871da72bf948bdac81111c7740c30b4c740f2a5c084b12fc604febe8913069acef51bf48ffd1a7135a269'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sfjourney::Application.config.session_store :cookie_store, key: '_sfjourney_session'
