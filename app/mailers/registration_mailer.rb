f432f19a79fc36fa15df209f3d6fc6467c0d1988
Thomas Lotze <lotze@squareup.com>
2013-09-27 10:57:04 -0700

    use gmail for sending email

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,5 +1,5 @@
 class RegistrationMailer < ActionMailer::Base
-  default :from => "Journey To The End Of The Night <jtteotn@spidere.com>"
+  default :from => "Journey To The End Of The Night <#{ENV['GMAIL_EMAIL']}>"
 
   def confirmation_email(registration)
     @registration = registration
