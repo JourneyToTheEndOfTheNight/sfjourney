429db120cd144f638e320c9109b4018102eee5a9
Thomas Lotze <lotze@squareup.com>
2013-10-01 16:52:21 -0700

    emailing on new registration creation

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,6 +1,12 @@
 class RegistrationMailer < ActionMailer::Base
   default :from => "Journey To The End Of The Night <#{ENV['GMAIL_EMAIL']}>"
 
+  def new_registration_email(registration)
+    @registration = registration
+    mail(:to => ENV['GMAIL_EMAIL'],
+         :subject => "[journeyreg] #{@registration.name}/#{@registration.team_name}; #{Registration.num_remaining} left")
+  end
+
   def confirmation_email(registration)
     @registration = registration
     mail(:to => @registration.email,
